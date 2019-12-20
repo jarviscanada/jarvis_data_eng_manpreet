ca2e3bb64d20c88eeed33687cbb060f85c441a7c
Manpreet Kaur <manpreetk0294@gmail.com>
2019-12-20 02:56:25 +0000

    Adding DOA

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/CrdDao.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/CrdDao.java
@@ -1,5 +1,9 @@
 package ca.jrvs.apps.twitter.dao;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
 public interface CrdDao<T, ID> {
 
     /**
@@ -7,19 +11,19 @@ public interface CrdDao<T, ID> {
      * @param entity entity that to be created
      * @return created entity
      */
-    T create(T entity);
+    T create(T entity) throws URISyntaxException, IOException;
 
     /**
      * Find an entity(Tweet) by its id
      * @param id entity id
      * @return Tweet entity
      */
-    T findById(ID id);
+    T findById(ID id) throws IOException, URISyntaxException;
 
     /**
      * Delete an entity(Tweet) by its ID
      * @param id of the entity to be deleted
      * @return deleted entity
      */
-    T deleteById(ID id);
+    T deleteById(ID id) throws IOException, URISyntaxException;
 }
