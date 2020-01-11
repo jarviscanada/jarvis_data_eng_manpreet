a9e28a8e83ab03923f09a61eb11cf849d940cae0
Manpreet Kaur <manpreetk0294@gmail.com>
2020-01-11 07:28:34 +0000

    letter with number

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/LetterWithNumber.java
@@ -0,0 +1,32 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * ticket: https://www.notion.so/Print-letter-with-number-ad0af19e6be0481796b8edbf53255fdd
+ */
+public class LetterWithNumber {
+
+    /**
+     * Big-O: Time complexity : O(n)
+     * Justification: The whole string is traversed.
+     *
+     **/
+    public String printLetterWithNumber(String input) {
+        StringBuilder newStr = new StringBuilder();
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException("Invalid Input: string conains spaces");
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) >= 97 && input.charAt(i) <= 122) {
+                newStr.append(input.charAt(i))
+                        .append(input.charAt(i) - 96);
+            } else if (input.charAt(i) >= 65 && input.charAt(i) <= 90) {
+                newStr.append(input.charAt(i))
+                        .append(input.charAt(i) - 64);
+            } else{
+                throw new IllegalArgumentException("Invalid Input");
+            }
+        }
+        return newStr.toString();
+    }
+
+}
