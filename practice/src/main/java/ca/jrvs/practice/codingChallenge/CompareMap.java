69fdd22b94a6033d46aafbefc051e811a33d80e2
Manpreet Kaur <manpreetk0294@gmail.com>
2020-01-15 14:57:24 +0000

    amending methods

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMap.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMap.java
@@ -8,11 +8,39 @@ import java.util.Map;
  */
 public class CompareMap {
 
-    public boolean compareMapUsingAPI(HashMap<Integer,String> map1, HashMap<Integer,String> map2){
+    /**
+     * Time Complexity: O(n)
+     * Justification: Implementation of equals have O(n) as iterator iterates through the map atmost once.
+     *
+     */
+    public <K,V> boolean compareMapUsingAPI(Map<K,V> map1, Map<K,V> map2){
         return map1.equals(map2);
     }
 
-    public boolean compareMap(HashMap<Integer,String> map1, HashMap<Integer,String> map2){
-        return false;
+    /**
+     * Time Complexity: O(n)
+     * Justification: Iterator iterates through the map atmost once.
+     *
+     */
+    public <K,V> boolean compareMap(Map<K,V> map1, Map<K,V> map2){
+
+            if(map1 == map2){
+                return true;
+            }
+
+            if (map1.size() != map2.size()) {
+                return false;
+            }
+
+            for (Map.Entry<K,V> pair : map1.entrySet()) {
+                if (!map2.containsKey(pair.getKey())) {
+                    return false;
+                }
+                if (!map2.get(pair.getKey()).equals(pair.getValue())) {
+                    return false;
+                }
+            }
+
+            return true;
     }
 }
--- practice/src/test/java/ca/jrvs/practice/codingChallenge/CompareMapTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/CompareMapTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import sun.tools.jar.CommandLine;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -12,9 +13,9 @@ public class CompareMapTest {
     @Test
     public void compareMapUsingAPI() {
         CompareMap maps = new CompareMap();
-        HashMap<Integer,String> map1 = new HashMap<>();
+        Map<Integer,String> map1 = new HashMap<>();
         map1.put(12,"name");
-        HashMap<Integer,String> map2 = new HashMap<>();
+        Map<Integer,String> map2 = new HashMap<>();
         map2.put(12,"yo");
         boolean actual = maps.compareMapUsingAPI(map1,map2);
         boolean expected = false;
@@ -23,5 +24,15 @@ public class CompareMapTest {
 
     @Test
     public void compareMap() {
+        CompareMap maps = new CompareMap();
+        Map<Integer,String> map1 = new HashMap<>();
+        map1.put(12,"name");
+        map1.put(13,"address");
+        Map<Integer,String> map2 = new HashMap<>();
+        map2.put(12,"name");
+        map2.put(13,"a1");
+        boolean actual = maps.compareMap(map1,map2);
+        boolean expected = false;
+        assertEquals(expected,actual);
     }
 }
