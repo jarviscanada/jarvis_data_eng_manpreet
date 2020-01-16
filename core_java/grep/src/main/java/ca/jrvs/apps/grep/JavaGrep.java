3cff72b53cf975cf9fdb653a2a8a5c0210276267
Manpreet1502 <49076016+Manpreet1502@users.noreply.github.com>
2020-01-15 19:00:57 -0500

    Update JavaGrep.java

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -9,16 +9,12 @@ public interface JavaGrep {
 
     void process() throws IOException;
 
-    /*traverse given directory and returns all files*/
     List<File> listFiles(String rootDir);
 
-    /*Read a file and return all the lines*/
     List<String> readLines(File inputFile);
 
-    /*check if a line contains the regex pattern which is passed by user*/
     boolean containsPattern(String line);
 
-    /*write lines to the file*/
     void writeToFile(List<String> lines) throws IOException;
 
     String getRootPath();
