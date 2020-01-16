7a74a7e86ac86fa7ed6e2767898e02abb964ef7b
Manpreet1502 <49076016+Manpreet1502@users.noreply.github.com>
2020-01-16 13:55:27 -0500

    Update and rename SolutionTest.java to FindDuplicateTest.java

--- practice/src/test/java/ca/jrvs/practice/codingChallenge/FindDuplicateTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/FindDuplicateTest.java
@@ -1,17 +1,15 @@
 package ca.jrvs.practice.codingChallenge;
 
 import org.junit.Test;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.junit.Assert.*;
 
-public class SolutionTest {
+public class FindDuplicateTest {
 
     @Test
     public void findDuplicateUsingSort() {
-        Solution obj = new Solution();
+        FindDuplicate obj = new FindDuplicate();
         int[] nums = {1,2,2,3,4};
         int actual = obj.findDuplicatesUsingSet(nums);
         int expected = 2;
@@ -20,7 +18,7 @@ public class SolutionTest {
 
     @Test
     public void findDuplicatesUsingSet() {
-        Solution obj = new Solution();
+        FindDuplicate obj = new FindDuplicate();
         int[] nums = {1,2,2,3,4};
         int actual = obj.findDuplicatesUsingSet(nums);
         int expected = 2;
