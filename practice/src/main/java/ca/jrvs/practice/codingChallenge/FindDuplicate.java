0fe7082ff07670a6fdb14b4e4daf5502670d18f6
Manpreet1502 <49076016+Manpreet1502@users.noreply.github.com>
2020-01-16 13:54:39 -0500

    Update and rename Solution.java to FindDuplicate.java

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/FindDuplicate.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/FindDuplicate.java
@@ -8,12 +8,15 @@ import java.util.Set;
  * ticket: https://www.notion.so/Find-the-Duplicate-Number-e00e7fd3d3954826b8a9e8986f3613ce
  */
 
-public class Solution {
+public class FindDuplicate {
 
     /**
-     * Time Complexity: O()
-     * Justification:
+     * Time Complexity: O(nlogn)
+     * Justification: sorting costs nlogn and iteration through loop costs O(n), Hence,
+     * total is O(nlogn)
      *
+     * Space Complexity:  O(1)
+     * Justification: No extra space used
      */
     public int findDuplicateUsingSort(int[] nums){
         Arrays.sort(nums);
@@ -26,9 +29,12 @@ public class Solution {
     }
 
     /**
-     * Time Complexity: O()
-     * Justification:
+     * Time Complexity: O(n)
+     * Justification: loop interation to nums length until n costs O(n)
      *
+     * Space Complexity:  O(n)
+     * Justification: set correspondes to an extra space that takes upto the length 
+     * of nums if there is no duplicate i.e. n
      */
     public int findDuplicatesUsingSet(int[] nums){
         Set<Integer> set = new HashSet<Integer>();
