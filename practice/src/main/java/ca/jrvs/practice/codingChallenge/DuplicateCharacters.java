3b574b43996ceefcc14eb1d21ef05a71203e678c
Manpreet Kaur <manpreetk0294@gmail.com>
2020-01-16 19:46:24 +0000

    adding DuplicateCharacters

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicateCharacters.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/DuplicateCharacters.java
@@ -1,7 +1,6 @@
 package ca.jrvs.practice.codingChallenge;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * ticket: https://www.notion.so/Duplicate-Characters-bb5f2398240746029b0d6aeb6cd123ab
@@ -9,31 +8,29 @@ import java.util.Set;
 public class DuplicateCharacters {
 
     /**
-     * Time Complexity: O()
-     * Justification:
+     * Time Complexity: O(n)
+     * Justification: Traverse over string once and traverse on Hashmap which implies complexity to be O(n+n)~O(n)
      *
-     * Space Complexity: O()
+     * Space Complexity: O(n)
+     * Justification: Extra space is required for HashMap and List which is O(n+n) = O(n)
      */
     public String[] findDuplicateCharacters(String inputStr){
-        Set<String> set = new HashSet<>(inputStr.length());
-        StringBuilder outStr = new StringBuilder();
-        String[] output = null;
+        HashMap<String, Integer> map = new HashMap<>();
+        inputStr = inputStr.toLowerCase();
 
-        // Creating array of string length
-        char[] ch = new char[inputStr.length()];
-
-        // Copy character by character into array
         for (int i = 0; i < inputStr.length(); i++) {
-            ch[i] = inputStr.charAt(i);
+            if (inputStr.charAt(i) < 'a' || inputStr.charAt(i) > 'z') continue;
+            String character = String.valueOf(inputStr.charAt(i));
+            map.put(character, map.getOrDefault(character, 0) + 1);
         }
 
-        for (char c : ch) {
-            outStr.append(c);
-            if (set.contains(c)) {
-                output = outStr.toString().split("");
-            }
+        List<String> duplicates = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) duplicates.add(entry.getKey());
         }
-        return output;
+
+        String[] duplicatesArray = duplicates.toArray(new String[duplicates.size()]);
+        return duplicatesArray;
 
     }
 }
